38e2729fce6f5fb0c4523712aa5e36a05f199640
Andreas Rozek <A.Rozek@gmx.de>
2021-06-15 09:10:29 +0200

    new build

--- svelte-agnostic-draggable.js
+++ svelte-agnostic-draggable.js
@@ -1,74 +1,62 @@
 /**** use:draggable/droppable/sortable={options} ****/
-
-  import agnosticDraggable from 'agnostic-draggable-bug-fixed'
-
+import agnosticDraggable from 'agnostic-draggable-bug-fixed';
 /**** use:draggable={options} ****/
-
-  const DraggableEventHandlers = {
-    'draggable:init':    InjectorForEvent('draggable:init'),
-    'drag:start':        InjectorForEvent('drag:start'),
-    'drag:move':         InjectorForEvent('drag:move'),
-    'drag:stop':         InjectorForEvent('drag:stop'),
+var DraggableEventHandlers = {
+    'draggable:init': InjectorForEvent('draggable:init'),
+    'drag:start': InjectorForEvent('drag:start'),
+    'drag:move': InjectorForEvent('drag:move'),
+    'drag:stop': InjectorForEvent('drag:stop'),
     'draggable:destroy': InjectorForEvent('draggable:destroy')
-  }
-
-  export function draggable (Element, Options) {
-    new agnosticDraggable.Draggable(Element,Options,DraggableEventHandlers)
-  }                    // currently, there is no possibility to update "Options"
-
+};
+export function draggable(draggableElement, Options) {
+    new agnosticDraggable.Draggable(draggableElement, Options, DraggableEventHandlers);
+} // currently, there is no possibility to update "Options"
 /**** use:droppable={options} ****/
-
-  const DroppableEventHandlers = {
-    'droppable:init':       InjectorForEvent('droppable:init'),
-    'droppable:activate':   InjectorForEvent('droppable:activate'),
-    'droppable:over':       InjectorForEvent('droppable:over'),
-    'droppable:drop':       InjectorForEvent('droppable:drop'),
-    'droppable:out':        InjectorForEvent('droppable:out'),
+var DroppableEventHandlers = {
+    'droppable:init': InjectorForEvent('droppable:init'),
+    'droppable:activate': InjectorForEvent('droppable:activate'),
+    'droppable:over': InjectorForEvent('droppable:over'),
+    'droppable:drop': InjectorForEvent('droppable:drop'),
+    'droppable:out': InjectorForEvent('droppable:out'),
     'droppable:deactivate': InjectorForEvent('droppable:deactivate'),
-    'droppable:destroy':    InjectorForEvent('droppable:destroy')
-  }
-
-  export function droppable (Element, Options) {
-    new agnosticDraggable.Droppable(Element,Options,DroppableEventHandlers)
-  }                    // currently, there is no possibility to update "Options"
-
+    'droppable:destroy': InjectorForEvent('droppable:destroy')
+};
+export function droppable(droppableElement, Options) {
+    new agnosticDraggable.Droppable(droppableElement, Options, DroppableEventHandlers);
+} // currently, there is no possibility to update "Options"
 /**** use:sortable={options} ****/
-
-  const SortableEventHandlers = {
-    'sortable:init':       InjectorForEvent('sortable:init'),
-    'sortable:activate':   InjectorForEvent('sortable:activate'),
-    'sort:start':          InjectorForEvent('sort:start'),
-    'sort:move':           InjectorForEvent('sort:move'),
-    'sort:stop':           InjectorForEvent('sort:stop'),
-    'sortable:over':       InjectorForEvent('sortable:over'),
-    'sortable:change':     InjectorForEvent('sortable:change'),
-    'sortable:remove':     InjectorForEvent('sortable:remove'),
-    'sortable:receive':    InjectorForEvent('sortable:receive'),
-    'sortable:update':     InjectorForEvent('sortable:update'),
-    'sortable:out':        InjectorForEvent('sortable:out'),
+var SortableEventHandlers = {
+    'sortable:init': InjectorForEvent('sortable:init'),
+    'sortable:activate': InjectorForEvent('sortable:activate'),
+    'sort:start': InjectorForEvent('sort:start'),
+    'sort:move': InjectorForEvent('sort:move'),
+    'sort:stop': InjectorForEvent('sort:stop'),
+    'sortable:over': InjectorForEvent('sortable:over'),
+    'sortable:change': InjectorForEvent('sortable:change'),
+    'sortable:remove': InjectorForEvent('sortable:remove'),
+    'sortable:receive': InjectorForEvent('sortable:receive'),
+    'sortable:update': InjectorForEvent('sortable:update'),
+    'sortable:out': InjectorForEvent('sortable:out'),
     'sortable:deactivate': InjectorForEvent('sortable:deactivate'),
-    'sortable:destroy':    InjectorForEvent('sortable:destroy')
-  }
-
-  export function sortable (Element, Options) {
-    new agnosticDraggable.Sortable(Element,Options,SortableEventHandlers)
-  }                    // currently, there is no possibility to update "Options"
-
+    'sortable:destroy': InjectorForEvent('sortable:destroy')
+};
+export function sortable(sortableElement, Options) {
+    new agnosticDraggable.Sortable(sortableElement, Options, SortableEventHandlers);
+} // currently, there is no possibility to update "Options"
 /**** InjectorForEvent ****/
-
-  function InjectorForEvent (Topic) {
-    return (originalEvent) => {
-      let EventData = originalEvent.data
-
-      ;[
-        EventData.source,
-        EventData.draggable && EventData.draggable.element,
-        EventData.droppable && EventData.droppable.element,
-        EventData.sortable  && EventData.sortable.element
-      ].forEach((Origin) => {
-        if (Origin != null) {
-          Origin.dispatchEvent(new CustomEvent(Topic, { detail:EventData }))
-        }
-      })
-    }
-  }
+function InjectorForEvent(Topic) {
+    return function (originalEvent) {
+        // @ts-ignore
+        var EventData = originalEvent.data;
+        [
+            EventData.source,
+            EventData.draggable && EventData.draggable.element,
+            EventData.droppable && EventData.droppable.element,
+            EventData.sortable && EventData.sortable.element
+        ].forEach(function (Origin) {
+            if (Origin != null) {
+                Origin.dispatchEvent(new CustomEvent(Topic, { detail: EventData }));
+            }
+        });
+    };
+}
