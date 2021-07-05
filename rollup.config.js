32e1d10b8eab714df6e3b45b0c6ec12a0b151576
Andreas Rozek <A.Rozek@gmx.de>
2021-07-05 08:26:08 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-// see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
-
 import commonjs   from '@rollup/plugin-commonjs'
 import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
