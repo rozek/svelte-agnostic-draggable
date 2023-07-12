00467d16ce5e95b404ff9ddf430736987afdb569
Andreas Rozek <A.Rozek@gmx.de>
2023-07-12 08:50:07 +0200

    new build

--- dist/src/svelte-agnostic-draggable.d.ts
+++
@@ -1,4 +0,0 @@
-/**** use:draggable/droppable/sortable={options} ****/
-export declare function draggable(draggableElement: Element, Options?: any): void;
-export declare function droppable(droppableElement: Element, Options?: any): void;
-export declare function sortable(sortableElement: Element, Options?: any): void;
